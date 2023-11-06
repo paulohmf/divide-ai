cf5b194bf0016a3c655328ce056eabe2c9da6620
Luis Monteiro <luis.monteiro@fluna.io>
2023-11-06 15:33:01 -0300

    Estilizando front com dados mockados.

--- client/src/pages/ExpensesRegister/ExpensesRegister.js
+++ client/src/pages/ExpensesRegister/ExpensesRegister.js
@@ -5,6 +5,14 @@ function Expenses() {
 
       //get expenses
   const [expenses, setExpenses] = useState([]);
+  const [newExpense, setNewExpense] = useState({
+    name: '',
+    teamName: '',
+    price: '',
+    // Add other fields as necessary
+});
+const [showForm, setShowForm] = useState(false);
+
 
    useEffect(() => {
       fetch('/api/expenses')
@@ -18,14 +26,44 @@ function Expenses() {
             console.log(err.message);
          });
    }, []);
-   
+
+   const handleInputChange = (event) => {
+    setNewExpense({
+        ...newExpense,
+        [event.target.name]: event.target.value
+    });
+};
+
    const handleNewExpense = () => {
-    // Handle new expense creation here
+
+    fetch('/api/expenses', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newExpense),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        setExpenses([...expenses, data]);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
 };
     return (
         <div>
-            <h1>Expenses</h1>
-            <button onClick={handleNewExpense}>Create New Expense</button>
+            <h1>Despesas</h1>
+            {!showForm && (<button onClick={() => setShowForm(!showForm)}>Criar nova despesa</button>)}
+            {showForm && (
+                <form onSubmit={handleNewExpense}>
+                    <input type="text" name="name" value={newExpense.name} onChange={handleInputChange} placeholder="Name" />
+                    <input type="text" name="teamName" value={newExpense.teamName} onChange={handleInputChange} placeholder="Team Name" />
+                    <input type="number" name="price" value={newExpense.price} onChange={handleInputChange} placeholder="Price" />
+                    {/* Add other fields as necessary */}
+                    <button type="submit">Submit</button>
+                </form>
+            )}
             <ul>
                 {expenses.map((expense, index) => (
                     <li key={index}>
@@ -34,9 +72,10 @@ function Expenses() {
                         <h3>Grupo da despesa: {expense.Team.name} </h3>
                         <h3>Valor: R$ {expense.price}</h3>
 
+                        <h3>Pessoas:</h3>
                         {expense.People.map( (person, personIndex) => (
                             <li key={personIndex}>
-                                <h4>{person.name}: R$ {person.ExpensePerson.share}</h4>
+                                <span>{person.name}: R$ {person.ExpensePerson.share}</span>
                             </li>
 
                         ))}
--- client/src/pages/Main/Main.js
+++ client/src/pages/Main/Main.js
@@ -50,27 +50,146 @@ function Main() {
   }
 
   return (
-    <div >
-      <div className='header'>
-        <div className="user-info">
-          <div className="user-name">Nome do Usuário</div>
-          <div className="user-avatar"></div>
+    // <div >
+    //   <div className='header'>
+    //     <div className="user-info">
+    //       <div className="user-name">Luís</div>
+    //       <div className="user-avatar"></div>
+    //     </div>
+    //     <button onClick={handleExpense} type="new-item-button">Despesas</button>
+    //   </div>
+    //   <div className = "group-list">
+    //     <div className="item-list">
+    //       <h4>Grupos de despesa</h4>
+    //       <ul>
+    //         <li>Padaria - Despesas pendentes</li>
+    //         <li>Viagem - Pago</li>
+    //         <li>Clube - Pago</li>
+    //       </ul>
+    //     </div>
+    //   </div>
+    //   <div className="div-button">
+    //     <button className="summary-button">Resumo</button>
+    //   </div>
+    // </div>
+
+    <div>
+        <div className="header-background-dashboard group">
+
+          <header className="header-dashboard">
+            <h1 className="header-logo-dashboard">Divide aí</h1>
+              <div className="header-list-center">
+                <ul className="header-list-dashboard">
+                  <li className="nav-button-logout" >Log Out</li>
+                  <li><div className="profile-pic-header"></div></li>
+                  {/* <li>{this.props.currentUser.username}</li> */}
+                </ul>
+              </div>
+          </header>
+
         </div>
-        <button onClick={handleExpense} type="new-item-button">Despesas</button>
-      </div>
-      <div className = "group-list">
-        <div className="item-list">
-          <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-          </ul>
+        <div className="dashboard-center-container">
+
+          <div className="dashboard-left">
+
+            {/* <FriendsContainer closeModal={this.closeFriendModal} isModalOpen={this.state.friendModalOpen} openModal ={this.openFriendModal}/> */}
+
+          </div>
+
+          <div className="dashboard-center">
+
+            {/* <SettleFormContainer closeModal={this.closeSettleModal} isModalOpen={this.state.settleModalOpen}/> */}
+
+            <section className="dashboard-top-section">
+              <section className="dashboard-top">
+
+                <h1>Dashboard</h1>
+
+                <ul className="dashboard-button-list">
+                  <li><button >Adicionar amigo</button></li>
+                  <li><button >Criar despesa</button></li>
+                  <li><button >Quitar Dívida</button></li>
+                </ul>
+
+              </section>
+
+              <section className="dashboard-bottom">
+                <div className="dashboard-block">
+                  <div className="title">Balanço total</div>
+                  <div className={"amount " + (10 > 0 ? "positive" : "negative")}>${36.57.toFixed(2)}</div>
+
+                </div>
+
+                <div className="dashboard-block">
+                  <div className="dashboard-block-border">
+
+                    <div className="title">Você deve</div>
+                    <div className="amount-owe">${24.46.toFixed(2)}</div>
+                  </div>
+
+                </div>
+
+                <div className="dashboard-block">
+                  <div className="title">Te devem</div>
+                  <div className="amount-owed">${20.00.toFixed(2)}</div>
+
+                </div>
+              </section>
+            </section>
+
+            <section className="payments-section">
+              <div className="owe-titles">
+                <div>Você Deve</div>
+                <div>Te devem</div>
+              </div>
+              <div className="you-owe-half">
+
+                {/* {isEmpty(this.props.bills.you_owe) ? (
+
+                  <div>You do not owe anything</div>
+                ) : (
+                  <ul>
+                    {youOweUsers}
+
+                  </ul>
+                )} */}
+
+              </div>
+
+              <div className="you-are-owed-half">
+
+                {/* {isEmpty(this.props.bills.you_are_owed) ? (
+
+                  <div>You are not owed anything</div>
+                ) : (
+                  <ul>
+                    {youAreOwedUsers}
+
+                  </ul>
+                )} */}
+
+              </div>
+
+
+
+            </section>
+
+          </div>
+
+          <div className="dashboard-right">
+
+          </div>
+
+
+
+
+          <div>
+
+            {/* <BillFormContainer closeModal={this.closeBillModal} isModalOpen={this.state.billModalOpen} /> */}
+          </div>
         </div>
       </div>
-      <div className="div-button">
-        <button className="summary-button">Resumo</button>
-      </div>
-    </div>
+    
   );
 }
 
